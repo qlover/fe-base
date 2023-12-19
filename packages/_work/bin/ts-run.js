9451d5cf96766580a234b564a7ab8df9c2d20883
qlover <myused@sina.com>
2023-12-19 11:20:24 +0800

    build(ts-run): add ts-run script

---
+++ packages/_work/bin/ts-run.js
@@ -0,0 +1,33 @@
+const { program } = require('commander')
+const { join } = require('path')
+const { rootPath } = require('../config/path.config')
+const { exec } = require('child_process')
+
+program.option('-p, --path', 'run script path')
+
+program.parse()
+
+function main() {
+  const path = program.args[0]
+
+  if (!path) {
+    console.log('path is undefined')
+    return
+  }
+
+  const scriptPath = join(rootPath, path)
+  const scriptCMD = `ts-node-esm ${scriptPath}`
+
+  // console.log('script path is', scriptPath)
+
+  exec(scriptCMD, (error, stdout, stderr) => {
+    if (error) {
+      console.log('[Error]', error)
+    } else {
+      console.log('[Success]')
+      console.log(stdout)
+    }
+  })
+}
+
+main()
