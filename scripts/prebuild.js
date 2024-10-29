d97de79e14bf11e1bb9b8c58b91a8d03b9bcf783
qlover <myused@sina.com>
2024-10-30 00:09:34 +0800

    chore: prebuild use fe-scripts

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,12 +1,10 @@
-import dotenv from 'dotenv';
+import { loadEnv } from '@qlover/fe-scripts/scripts';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-dotenv.config({
-  path: [join(__dirname, '../.env.local'), join(__dirname, '../.env')]
-});
+loadEnv(join(__dirname, '../'));
 
 console.log('Enveronment is', process.env.NODE_ENV);
