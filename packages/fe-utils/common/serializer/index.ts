9fcfa1adc336a75851b5d057540262af7514b2a3
qlover <myused@sina.com>
2024-11-30 16:07:19 +0800

    feat: Serializer JSON and Base64

---
+++ packages/fe-utils/common/serializer/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Export serialization interface and implementations
+ * @module serializer
+ */
+export * from './Serializer';
+export * from './JSONSerializer';
+export * from './Base64Serializer';
