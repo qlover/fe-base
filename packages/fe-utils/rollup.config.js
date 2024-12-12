f878715a7e01f3f6f3f233f10e8834d6819569ff
QRJ <renjie.qin@brain.im>
2024-12-12 09:13:47 +0800

    chore: clean dist

--- packages/fe-utils/rollup.config.js
+++ packages/fe-utils/rollup.config.js
@@ -4,6 +4,7 @@ import typescript from 'rollup-plugin-typescript2';
 import dts from 'rollup-plugin-dts';
 import terser from '@rollup/plugin-terser';
 import { searchEnv } from '@qlover/fe-scripts/scripts/search-env.js';
+import del from 'rollup-plugin-delete';
 
 const env = searchEnv({ logger: console });
 const NODE_ENV = env.get('NODE_ENV');
@@ -12,10 +13,18 @@ console.log('Enveronment is', NODE_ENV);
 
 const serverExternal = ['crypto', 'buffer', 'zlib', 'axios'];
 const commonExternal = ['axios'];
+const buildDir = 'dist';
 
-function createBuilder(entry, formats, external) {
+/**
+ * @param {{ entry: string, formats: string[], external: string[], target: string, clean: boolean }} options
+ * @returns {import('rollup').RollupOptions[]}
+ */
+function createBuilder({ target, entry, formats, external, clean }) {
+  target = target || `${buildDir}/${entry}`;
+
+  /** @type {import('rollup').OutputOptions[]} */
   const outputs = formats.map((format) => ({
-    file: `dist/${entry}/index.${format}`,
+    file: `${target}/index.${format}.js`,
     format,
     name:
       format === 'umd'
@@ -29,6 +38,7 @@ function createBuilder(entry, formats, external) {
       input: `${entry}/index.ts`,
       output: outputs,
       plugins: [
+        clean && del({ targets: `${buildDir}/*` }),
         resolve({
           preferBuiltins: false
         }),
@@ -41,7 +51,7 @@ function createBuilder(entry, formats, external) {
     {
       input: `${entry}/index.ts`,
       output: {
-        file: `dist/${entry}/index.d.ts`,
+        file: `${target}/index.d.ts`,
         format: 'es'
       },
       plugins: [dts()]
@@ -53,7 +63,20 @@ function createBuilder(entry, formats, external) {
  * @type {import('rollup').RollupOptions[]}
  */
 export default [
-  ...createBuilder('interface', ['cjs', 'esm', 'umd'], []),
-  ...createBuilder('common', ['cjs', 'esm', 'umd'], commonExternal),
-  ...createBuilder('server', ['cjs', 'esm'], serverExternal)
+  ...createBuilder({
+    entry: 'interface',
+    formats: ['cjs', 'esm', 'umd'],
+    clean: true
+  }),
+  ...createBuilder({
+    entry: 'common',
+    target: buildDir,
+    formats: ['cjs', 'esm', 'umd'],
+    external: commonExternal
+  }),
+  ...createBuilder({
+    entry: 'server',
+    formats: ['cjs', 'esm'],
+    external: serverExternal
+  })
 ];
