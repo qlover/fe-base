1d88844fe2784adce0c928cfd890bddd6378c7e8
QRJ <renjie.qin@brain.im>
2024-12-16 14:59:38 +0800

    chore: remove eslint comment

--- eslint.config.js
+++ eslint.config.js
@@ -67,16 +67,6 @@ export default [
   createCommon(),
   createTslintRecommended(['packages/**/*.ts']),
 
-  // {
-  //   name: 'fe-scripts',
-  //   files: ['packages/fe-scripts/**/*.ts'],
-  //   languageOptions: {
-  //     globals: {
-  //       ...globals.node
-  //     }
-  //   }
-  // },
-
   // fe-utils
   ...createFeUtilsConfig(),
 
