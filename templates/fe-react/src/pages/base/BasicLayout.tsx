9ca9e5eac52c8281375e3ecdc14237446cefcccf
QRJ <renjie.qin@brain.im>
2025-01-02 08:56:40 +0800

    chore: add baes overried theme colors

--- templates/fe-react/src/pages/base/BasicLayout.tsx
+++ templates/fe-react/src/pages/base/BasicLayout.tsx
@@ -3,13 +3,15 @@ import BaseHeader from './components/BaseHeader';
 import { Outlet } from 'react-router-dom';
 
 export default function BasicLayout() {
-
   useLanguageGuard();
 
   return (
     <div data-testid="basic-layout" className="text-base">
       <BaseHeader />
-      <Outlet />
+
+      <div className="text-black bg-white">
+        <Outlet />
+      </div>
     </div>
   );
 }
