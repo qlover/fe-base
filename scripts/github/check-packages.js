863559ad1955ade13a921b7d49a2f4719b717ecf
qlover <myused@sina.com>
2025-01-10 21:00:01 +0800

    test: general log

--- scripts/github/check-packages.js
+++ scripts/github/check-packages.js
@@ -1,56 +1,61 @@
 #!/usr/bin/env node
 
-import { execSync } from 'child_process';
-import fs from 'fs';
-import path from 'path';
+// import { execSync } from 'child_process';
+// import fs from 'fs';
+// import path from 'path';
 
 function main() {
   const baseRef = process.argv[2];
 
-  console.log(`jj baseRef: ${baseRef}`);
-
-  if (!baseRef) {
-    return;
-  }
-
-  const config = JSON.parse(
-    fs.readFileSync(path.resolve(process.cwd(), 'fe-config.json'), 'utf-8')
+  console.log(
+    `jj baseRef : ${baseRef} process.argv: ${JSON.stringify(process.argv || [])}`
   );
+  return;
 
-  console.log(`jj config: ${config}`);
+  // console.log(`jj baseRef: ${baseRef}`);
 
-  const directories = config.releasePackages;
+  // if (!baseRef) {
+  //   return;
+  // }
 
-  console.log(`jj directories: ${directories}`);
+  // const config = JSON.parse(
+  //   fs.readFileSync(path.resolve(process.cwd(), 'fe-config.json'), 'utf-8')
+  // );
 
-  if (directories.length === 0) {
-    console.log('no release packages');
-    return;
-  }
+  // console.log(`jj config: ${config}`);
 
-  const changedPackages = [];
-  directories.forEach((dir) => {
-    try {
-      const changedFiles = execSync(
-        `git diff --name-only origin/${baseRef} HEAD`
-      ).toString();
-      console.log(`jj changedFiles: ${changedFiles}`);
+  // const directories = config.releasePackages;
 
-      // 使用 JavaScript 过滤文件名
-      const filteredFiles = changedFiles
-        .split('\n')
-        .filter((file) => file.startsWith(`packages/${dir}/`));
-      if (filteredFiles.length > 0) {
-        changedPackages.push(dir.replace('-', '_')); // 记录发生变化的包
-      }
-    } catch {
-      // 如果没有变化，捕获错误并继续
-    }
-  });
-  // 输出发生变化的包
-  if (changedPackages.length > 0) {
-    console.log(`changed_packages=${changedPackages.join(',')}`); // 输出变化的包
-  }
+  // console.log(`jj directories: ${directories}`);
+
+  // if (directories.length === 0) {
+  //   console.log('no release packages');
+  //   return;
+  // }
+
+  // const changedPackages = [];
+  // directories.forEach((dir) => {
+  //   try {
+  //     const changedFiles = execSync(
+  //       `git diff --name-only origin/${baseRef} HEAD`
+  //     ).toString();
+  //     console.log(`jj changedFiles: ${changedFiles}`);
+
+  //     // 使用 JavaScript 过滤文件名
+  //     const filteredFiles = changedFiles
+  //       .split('\n')
+  //       .filter((file) => file.startsWith(`packages/${dir}/`));
+  //     if (filteredFiles.length > 0) {
+  //       changedPackages.push(dir.replace('-', '_')); // 记录发生变化的包
+  //     }
+  //   } catch {
+  //     // 如果没有变化，捕获错误并继续
+  //   }
+  // });
+  // // 输出发生变化的包
+  // if (changedPackages.length > 0) {
+  //   console.log(`changed_packages=${changedPackages.join(',')}`); // 输出变化的包
+  // }
 }
 
 main();
