dcc2403ff9ff1abee9318fa98ed9da9b1e8056a2
qlover <myused@sina.com>
2025-01-10 20:55:29 +0800

    test: general log

--- scripts/github/check-packages.js
+++ scripts/github/check-packages.js
@@ -17,7 +17,11 @@ function main() {
     fs.readFileSync(path.resolve(process.cwd(), 'fe-config.json'), 'utf-8')
   );
 
+  console.log('jj config', config);
+
   const directories = config.releasePackages;
+
+  console.log('jj directories', directories);
 
   if (directories.length === 0) {
     console.log('no release packages');
@@ -30,6 +34,8 @@ function main() {
       const changedFiles = execSync(
         `git diff --name-only origin/${baseRef} HEAD`
       ).toString();
+      console.log('jj changedFiles', changedFiles);
+
       // 使用 JavaScript 过滤文件名
       const filteredFiles = changedFiles
         .split('\n')
