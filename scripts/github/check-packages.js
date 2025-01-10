c9eca38d89ea039638d093f966827a59fa36c735
qlover <myused@sina.com>
2025-01-10 21:53:00 +0800

    feat: check packages

--- scripts/github/check-packages.js
+++ scripts/github/check-packages.js
@@ -1,60 +1,87 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
 
-// import { execSync } from 'child_process';
-// import fs from 'fs';
-// import path from 'path';
+import { execSync } from 'child_process';
+import fs from 'fs';
+import { dirname, resolve } from 'path';
 import { githubLog } from './log.js';
+import { fileURLToPath } from 'url';
 
-function main() {
-  const baseRef = process.argv[2];
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const feConfigPath = resolve(__dirname, '../../fe-config.json');
+const feConfig = JSON.parse(fs.readFileSync(feConfigPath, 'utf-8'));
 
-  githubLog(baseRef, 'baseRef');
-  return;
+function getChangePackageNames(barnch) {
+  const changedFiles = execSync(
+    `git diff --name-only origin/${barnch} HEAD`
+  ).toString();
 
-  // console.log(`jj baseRef: ${baseRef}`);
+  // get real files changed
+  const filteredFiles = changedFiles.split('\n');
 
-  // if (!baseRef) {
-  //   return;
-  // }
+  const releasePackages = feConfig.releasePackages || [];
 
-  // const config = JSON.parse(
-  //   fs.readFileSync(path.resolve(process.cwd(), 'fe-config.json'), 'utf-8')
-  // );
+  const result = releasePackages.reduce((acc, name) => {
+    acc[name] = false;
+    return acc;
+  }, {});
 
-  // console.log(`jj config: ${config}`);
+  filteredFiles.forEach((file) => {
+    releasePackages.forEach((name) => {
+      if (file.startsWith(name)) {
+        result[name] = true;
+      }
+    });
+  });
 
-  // const directories = config.releasePackages;
-
-  // console.log(`jj directories: ${directories}`);
-
-  // if (directories.length === 0) {
-  //   console.log('no release packages');
-  //   return;
-  // }
-
-  // const changedPackages = [];
-  // directories.forEach((dir) => {
-  //   try {
-  //     const changedFiles = execSync(
-  //       `git diff --name-only origin/${baseRef} HEAD`
-  //     ).toString();
-  //     console.log(`jj changedFiles: ${changedFiles}`);
-
-  //     // 使用 JavaScript 过滤文件名
-  //     const filteredFiles = changedFiles
-  //       .split('\n')
-  //       .filter((file) => file.startsWith(`packages/${dir}/`));
-  //     if (filteredFiles.length > 0) {
-  //       changedPackages.push(dir.replace('-', '_')); // 记录发生变化的包
-  //     }
-  //   } catch {
-  //     // 如果没有变化，捕获错误并继续
-  //   }
-  // });
-  // // 输出发生变化的包
-  // if (changedPackages.length > 0) {
-  //   console.log(`changed_packages=${changedPackages.join(',')}`); // 输出变化的包
-  // }
+  return Object.entries(result)
+    .filter(([, value]) => value)
+    .map(([key]) => key);
 }
 
-main();
+async function addChangePackagePRLables(changePackageNames) {
+  const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_REF } = process.env;
+  const [owner, repo] = GITHUB_REPOSITORY.split('/');
+
+  const releasePRLabelTemplate =
+    feConfig.releasePRLabelTemplate || 'pkg:${name}';
+  const labels = changePackageNames.map((name) =>
+    releasePRLabelTemplate.replace('${name}', name)
+  );
+  if (labels.length > 0) {
+    const { Octokit } = await import('@octokit/rest');
+    const octokit = new Octokit({ auth: GITHUB_TOKEN });
+    // get issue number
+    const issueNumber = GITHUB_REF.split('/').pop();
+    await octokit.issues.addLabels({
+      owner,
+      repo,
+      issue_number: issueNumber,
+      labels
+    });
+  }
+}
+
+async function main() {
+  const { GITHUB_TOKEN, GITHUB_REPOSITORY, GITHUB_REF } = process.env;
+
+  if (!GITHUB_TOKEN || !GITHUB_REPOSITORY || !GITHUB_REF) {
+    return;
+  }
+
+  const baseRef = process.argv[2];
+
+  if (!baseRef) {
+    return;
+  }
+
+  const changePackageNames = getChangePackageNames(baseRef);
+
+  if (changePackageNames.length > 0) {
+    await addChangePackagePRLables(changePackageNames);
+    githubLog('success!', 'addLables');
+  }
+}
+
+main().catch(() => {
+  githubLog('error!', 'addLables');
+});
