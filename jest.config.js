41799992b686729b759ee064b099a8531ed0098e
QRJ <renjie.qin@brain.im>
2025-01-10 17:06:26 +0800

    chore: add test root path

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ import tsJestConfig from '@qlover/fe-standard/config/jest.esm.js';
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
-  ...tsJestConfig
+  ...tsJestConfig,
+  testMatch: tsJestConfig.testMatch.map((item) => '<rootDir>/packages/' + item)
 };
