24562f9f414bf75df27db1b577602492a6493083
QRJ <renjie.qin@brain.im>
2025-01-14 16:28:09 +0800

    chore: add tsbuildinfo ignore

--- packages/create-fe-app/bin/create-fe-app.js
+++ packages/create-fe-app/bin/create-fe-app.js
@@ -3,15 +3,22 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import { Generator } from '../dist/es/index.js';
+import { existsSync } from 'fs';
 
 async function main() {
   const __dirname = dirname(fileURLToPath(import.meta.url));
+  const templatePath = join(__dirname, '../templates');
 
   const options = {
     options: {
-      templatePath: join(__dirname, '../../../templates')
+      templatePath
     }
   };
+
+  if (!existsSync(templatePath)) {
+    console.error('Template is empty!');
+    process.exit(1);
+  }
 
   const generator = new Generator(options);
 
