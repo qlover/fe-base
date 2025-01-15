69b845bbfa24a98c1045aef910aa95e3765240ee
qlover <myused@sina.com>
2025-01-15 22:38:11 +0800

    fix: add config

--- packages/create-app/templates/react-vite-lib/eslint.config.js
+++ packages/create-app/templates/react-vite-lib/eslint.config.js
@@ -3,9 +3,10 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import * as feDev from '@qlover/eslint-plugin-fe-dev';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -15,14 +16,16 @@ export default tseslint.config(
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
+      'react-refresh': reactRefresh,
+      'fe-dev': feDev
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true }
-      ]
+      ],
+      'fe-dev/ts-class-method-return': 'error'
     }
   }
 );
