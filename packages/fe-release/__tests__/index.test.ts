e743ac3eb49c6012f2c94c73a0c4b55c7ff57f6c
QRJ <renjie.qin@brain.im>
2025-02-17 11:35:08 +0800

    test: add mock GITHUB_TOKEN var

--- packages/fe-release/__tests__/index.test.ts
+++ packages/fe-release/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, beforeEach, it, expect, vi } from 'vitest';
+import { describe, beforeEach, it, expect, vi, afterEach } from 'vitest';
 import { release } from '../src/index';
 import { ReleaseItInstanceType } from '../src/type';
 
@@ -6,7 +6,13 @@ describe('index', () => {
   let releaseIt: ReleaseItInstanceType;
 
   beforeEach(() => {
+    process.env.GITHUB_TOKEN = 'mocked_github_token';
     releaseIt = vi.fn();
+  });
+
+  afterEach(() => {
+    delete process.env.GITHUB_TOKEN;
+    delete process.env.PAT_TOKEN;
   });
 
   it('should throw an error if packageJson is not provided', async () => {
