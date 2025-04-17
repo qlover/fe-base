aa7447b51a3dc755689de0783e8fd211ff99672e
qlover <473017871@qq.com>
2025-04-17 14:52:00 +0800

    fix: fe-relesae build cli (#325)
    * fix: fe-relesae build cli
    
    * fix: loader load test
    
    ---------
    
    Co-authored-by: QRJ <renjie.qin@brain.im>

--- packages/fe-release/rollup.config.js
+++ packages/fe-release/rollup.config.js
@@ -2,5 +2,9 @@ import { createBaseRollup } from '../../make/createBaseRollup.js';
 import { Env } from '@qlover/env-loader';
 
 export default createBaseRollup({
+  input: {
+    index: 'src/index.ts',
+    cli: 'src/cli.ts'
+  },
   isProduction: Env.searchEnv().get('NODE_ENV') === 'production'
 });
