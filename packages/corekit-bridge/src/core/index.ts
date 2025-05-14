bc7946f5fc1c8abc4ba258d7a5afe3c2bb1636f8
qlover <myused@sina.com>
2025-05-14 12:35:26 +0800

    feat(corekit-bridge): enhance build configuration and export structure
    - Updated Rollup configuration to utilize createBaseRollup for streamlined builds.
    - Added new TypeScript configuration files for building and ESM support.
    - Modified package.json to include main and module entries for better module resolution.
    - Expanded exports in core index file to include additional dependencies.
    - Refactored ColorFormatter to improve method visibility and added override annotation.

--- packages/corekit-bridge/src/core/index.ts
+++ packages/corekit-bridge/src/core/index.ts
@@ -5,3 +5,8 @@ export * from './request-plugins';
 export * from './storage-token';
 export * from './theme-service';
 export * from './thread';
+
+// export all the dependencies
+export * from '@qlover/logger';
+export * from '@qlover/fe-corekit';
+export * from '@qlover/slice-store-react';
