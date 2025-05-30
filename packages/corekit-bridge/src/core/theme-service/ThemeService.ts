c323edf70016a10b016cc470c264a978eeba015b
qlover <473017871@qq.com>
2025-05-30 16:31:15 +0800

    Update corekit bridge (#406)
    * refactor(theme-service): enhance theme configuration and remove unused tw-generator
    
    - Introduced a default theme configuration with properties for DOM attribute, default theme, target element, supported themes, storage key, initialization, and store prioritization.
    - Updated ThemeService to utilize the new configuration and improved theme binding logic.
    - Enhanced ThemeStateGetter to conditionally access storage based on the new prioritizeStore property.
    - Removed the unused tw-generator file to streamline the theme service module.
    
    * refactor(theme-service): add cacheTarget option and improve target element handling
    
    - Introduced a new cacheTarget property in the theme configuration to optimize target element retrieval.
    - Enhanced ThemeService to cache the target element for improved performance.
    - Updated bindToTheme method to utilize the new getTarget method for better clarity and efficiency.
    - Removed redundant storage handling from changeTheme method to streamline theme updates.
    
    ---------
    
    Co-authored-by: QRJ <renjie.qin@brain.im>

--- packages/corekit-bridge/src/core/theme-service/ThemeService.ts
+++ packages/corekit-bridge/src/core/theme-service/ThemeService.ts
@@ -1,16 +1,54 @@
 import { SliceStore } from '@qlover/slice-store-react';
-import { ThemeServiceProps, ThemeServiceState } from './type';
+import { ThemeConfig, ThemeServiceProps, ThemeServiceState } from './type';
 import { ThemeStateGetter } from './ThemeStateGetter';
 
-export class ThemeService extends SliceStore<ThemeServiceState> {
-  constructor(private props: ThemeServiceProps) {
-    super(() => ThemeStateGetter.create(props));
+export const defaultThemeConfig: ThemeConfig = {
+  domAttribute: 'data-theme',
+  defaultTheme: 'system',
+  target: 'html',
+  supportedThemes: ['light', 'dark'],
+  storageKey: 'theme',
+  init: true,
+  prioritizeStore: true,
+  cacheTarget: true
+};
 
-    this.bindToTheme();
+export class ThemeService extends SliceStore<ThemeServiceState> {
+  private _target: HTMLElement | null = null;
+
+  constructor(private props: ThemeServiceProps) {
+    const config = { ...defaultThemeConfig, ...props };
+
+    super(() => ThemeStateGetter.create(config));
+
+    if (config.init) {
+      this.bindToTheme();
+    }
   }
 
   getSupportedThemes(): string[] {
-    return this.props.supportedThemes;
+    return this.props.supportedThemes!;
+  }
+
+  getTarget(): HTMLElement {
+    const { target, cacheTarget } = this.props;
+
+    if (cacheTarget && this._target) {
+      return this._target;
+    }
+
+    let targetElement: HTMLElement;
+    if (target instanceof HTMLElement) {
+      targetElement = target;
+    } else if (typeof target === 'string') {
+      targetElement = document.querySelector(target) as HTMLElement;
+    } else {
+      targetElement = document.documentElement;
+    }
+
+    this._target = targetElement;
+
+    return targetElement;
   }
 
   bindToTheme(): void {
@@ -18,8 +56,15 @@ export class ThemeService extends SliceStore<ThemeServiceState> {
 
     const { domAttribute } = this.props;
 
+    const targetElement = this.getTarget();
+
     if (domAttribute) {
-      document.documentElement.setAttribute(domAttribute, theme);
+      targetElement.setAttribute(domAttribute, theme);
+    }
+
+    const { storage, storageKey } = this.props;
+    if (storage && storageKey) {
+      storage.setItem(storageKey, theme);
     }
   }
 
@@ -29,11 +74,6 @@ export class ThemeService extends SliceStore<ThemeServiceState> {
     }
 
     this.emit({ ...this.state, theme });
-
-    const { storage, storageKey } = this.props;
-    if (storage && storageKey) {
-      storage.setItem(storageKey, theme);
-    }
 
     this.bindToTheme();
   }
--- packages/corekit-bridge/src/core/theme-service/type.ts
+++ packages/corekit-bridge/src/core/theme-service/type.ts
@@ -5,17 +5,59 @@ export type ThemeServiceState = {
 };
 
 export type ThemeConfig = {
-  domAttribute: string;
-  defaultTheme: string;
-  target: string;
-  supportedThemes: string[];
-  storageKey: string;
-  styleThemeKeyTemplate: string;
-  styleKeyTemplate: string;
-  colorsValueTemplate: string;
-  colors: Record<string, unknown>;
+  /**
+   * The dom attribute
+   * @default 'data-theme'
+   */
+  domAttribute?: string;
+
+  /**
+   * The default theme
+   * @default 'system'
+   */
+  defaultTheme?: string;
+  /**
+   * The target element
+   *
+   * - Dom id(# startwith)
+   * - Dom Element
+   * - Dom Tag name
+   *
+   * @default 'html'
+   */
+  target?: string | HTMLElement;
+  /**
+   * The supported themes
+   *
+   * @default ['light', 'dark']
+   */
+  supportedThemes?: string[];
+  /**
+   * The storage key
+   * @default 'theme'
+   */
+  storageKey?: string;
+
+  /**
+   * Whether to initialize the theme
+   * @default `true`
+   */
+  init?: boolean;
+
+  /**
+   * Whether to prioritize the store
+   * @default `true`
+   */
+  prioritizeStore?: boolean;
+
+  /**
+   * Whether to cache the target
+   * @default `true`
+   */
+  cacheTarget?: boolean;
 };
 
 export interface ThemeServiceProps extends ThemeConfig {
+  /** */
   storage?: JSONStorage;
 }
