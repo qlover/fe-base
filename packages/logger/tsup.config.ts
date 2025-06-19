024731473b1acb019ddb4fa8ea526e31e54de96c
QRJ <renjie.qin@brain.im>
2025-06-19 16:51:50 +0800

    feat(logger): update logger package configuration and add project setup
    - Changed the main entry point in `package.json` from `index.cjs` to `index.umd.js` for better compatibility.
    - Updated the homepage link in `package.json` to point directly to the logger package documentation.
    - Introduced a new `project.json` file to define the logger package structure and build targets using NX.
    - Modified `tsup.config.ts` to adjust output formats and enable minification, enhancing the build process for the logger package.
    
    These changes improve the configuration and usability of the logger package within the monorepo.

--- packages/logger/tsup.config.ts
+++ packages/logger/tsup.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'tsup';
+import pkg from './package.json';
+import { toPureCamelCase } from '../../make/toPureCamelCase';
 
+const pkgName = toPureCamelCase(pkg.name);
 export default defineConfig([
   {
     entry: ['src/index.ts'],
-    format: ['esm', 'cjs'],
+    format: ['cjs', 'iife'],
     dts: false,
-    sourcemap: true,
+    minify: true,
     clean: true,
-    minify: process.env.NODE_ENV === 'production',
+    silent: true,
+    globalName: pkgName,
+    outExtension: ({ format }) => {
+      if (format === 'iife') {
+        return { js: '.iife.js' };
+      }
+      return { js: '.cjs' };
+    },
     outDir: 'dist'
   },
   {
