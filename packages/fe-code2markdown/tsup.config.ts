a7c00314803bc288657d765c9ddb91beca0a4a51
QRJ <renjie.qin@brain.im>
2025-06-20 10:36:06 +0800

    feat(fe-code2markdown): migrate build process to tsup and add project configuration
    - Updated the main entry point in `package.json` to use `index.js` for consistency with ESM.
    - Replaced the build script in `package.json` to utilize `tsup`, enhancing the build process.
    - Introduced a new `project.json` file to define build targets and improve integration with NX.
    - Created a new `tsup.config.ts` file for configuring the build process, supporting both ESM and CommonJS formats.
    - Removed the obsolete `rollup.config.js` file to streamline the project structure.
    
    These changes enhance the build orchestration and configuration for the fe-code2markdown package.

---
+++ packages/fe-code2markdown/tsup.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'tsup';
+import { builtinModules } from 'module';
+import pkg from './package.json';
+const external = [
+  ...builtinModules,
+  ...builtinModules.map((mod) => `node:${mod}`),
+  ...Object.keys(pkg.dependencies || {})
+];
+
+export default defineConfig([
+  {
+    entry: ['src/index.ts', 'src/cli.ts'],
+    format: 'esm',
+    dts: false,
+    clean: true,
+    minify: true,
+    external,
+    platform: 'node'
+  },
+  {
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    dts: true
+  }
+]);
