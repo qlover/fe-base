47dbd7222a6a8333303a02624f4ec085efd89f94
QRJ <github-actions[bot]@users.noreply.github.com>
2025-11-21 15:12:50 +0800

    refactor(fe-corekit): reorganize dependencies and enhance tsup configuration
    - Moved '@types/lodash' to devDependencies in package.json for better dependency management.
    - Updated pnpm-lock.yaml to reflect the new structure of dependencies.
    - Enhanced tsup configuration by adding external modules and a new IIFE build format, improving the build process and output management.
    
    These changes aim to streamline the dependency structure and optimize the build configuration for better performance and maintainability.

--- packages/fe-corekit/tsup.config.ts
+++ packages/fe-corekit/tsup.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'tsup';
 import pkg from './package.json';
 import { toPureCamelCase } from '../../make/toPureCamelCase';
+import { builtinModules } from 'node:module';
+
+const external = [
+  ...builtinModules,
+  ...builtinModules.map((mod) => `node:${mod}`),
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.devDependencies || {})
+];
 
 const pkgName = toPureCamelCase(pkg.name);
 export default defineConfig([
@@ -8,7 +16,6 @@ export default defineConfig([
     entry: ['src/index.ts'],
     format: ['cjs', 'iife'],
     dts: false,
-    minify: true,
     clean: true,
     silent: true,
     globalName: pkgName,
@@ -22,8 +29,21 @@ export default defineConfig([
   },
   {
     entry: ['src/index.ts'],
+    format: ['iife'],
+    dts: false,
+    minify: true,
+    silent: true,
+    globalName: pkgName,
+    outExtension: () => {
+      return { js: '.iife.min.js' };
+    },
+    outDir: 'dist'
+  },
+  {
+    entry: ['src/index.ts'],
     format: 'esm',
     dts: true,
-    outDir: 'dist'
+    outDir: 'dist',
+    external
   }
 ]);
